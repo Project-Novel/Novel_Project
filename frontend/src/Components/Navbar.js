39af8a950980496d42664d9105082238669c3d33
jeay123@naver.com <jeay123@naver.com>
2021-07-03 19:36:34 +0900

    first

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -7,20 +7,19 @@ function Navbar() {
     const [showLinks, setShowLinks] = useState(false);
     return (
         <div className="Navbar">
-         <div ClassName="leftSide">
-             <div className="links" id={showLinks ? "hidden" : ""}>
-             <a href="#">Home</a>
-             <a href="#">Feedback</a>
-             <a href="#">About Us</a>
-             <a href="#">Contact</a>
-             </div>
-             <button onClick={()=> setShowLinks(!showLinks)}>Open</button>
-            </div>   
-         <div ClassName="rightSide">
-             <input type="text" placeholder="Search..."/>
-             <button>Search</button>
-             </div>  
-
+         <div className="leftSide">
+            <div className="links" id={showLinks ? "hidden" : ""}>
+            <a href="/">Home</a>
+            <a href="#">Feedback</a>
+            <a href="#">About Us</a>
+            <a href="#">Contact</a>
+            </div>
+            <button onClick={()=> setShowLinks(!showLinks)}>Open</button>
+        </div>   
+         <div className="rightSide">
+            <input type="text" placeholder="Search..."/>
+            <button>Search</button>
+            </div>  
         </div>
     )
 }
